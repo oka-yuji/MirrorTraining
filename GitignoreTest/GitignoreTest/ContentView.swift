32f44ede9b375ee4515fdef08feb130b874c42a1
岡優志 <yu.j.0513@gmail.com>
2021-06-29 23:49:48 +0900

    プロジェクト作成

---
+++ GitignoreTest/GitignoreTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitignoreTest
+//
+//  Created by 岡優志 on 2021/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitignoreTest/GitignoreTest/GitignoreTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitignoreTestApp.swift
+//  GitignoreTest
+//
+//  Created by 岡優志 on 2021/06/29.
+//
+
+import SwiftUI
+
+@main
+struct GitignoreTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
